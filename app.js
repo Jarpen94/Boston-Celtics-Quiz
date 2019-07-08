e39b237ef4b28a02a92c9ce7c8ff01de28900352
Jarpen94 <kwiatkowskijarek@o2.pl>
2019-07-08 14:01:58 +0200

    animated score

--- app.js
+++ app.js
@@ -16,8 +16,17 @@ form.addEventListener('submit', e => {
     })
 
     //show result on page
-    scrollTo(0,0)
-    result.querySelector('span').textContent = `${score}%`
+    scrollTo(0, 0)
     result.classList.remove('d-none')
+
+    let output = 0
+    const timer = setInterval(() =>{
+        result.querySelector('span').textContent = `${output}%`
+        if(output === score){
+            clearInterval(timer)
+        } else {
+            output++
+        }
+    }, 10)
 
 })
